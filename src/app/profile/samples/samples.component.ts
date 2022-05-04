01b26cf1155eba6cd6c7a99122c5db9588272254
jakiebabe <jakerebullo@gmail.com>
2022-05-04 13:12:06 +0800

    updated readme

--- src/app/profile/samples/samples.component.ts
+++ src/app/profile/samples/samples.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SamplesComponent implements OnInit {
 
+  title: string = 'Sample Outputs';
   constructor() { }
 
   ngOnInit(): void {
--- src/app/profile/skills/skills.component.ts
+++ src/app/profile/skills/skills.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  title: string = 'Skills';
   constructor() { }
 
   ngOnInit(): void {
